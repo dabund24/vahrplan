29ac736298d4019dcf5ad11a67b7c7a87d7845e1
dabund24 <dabund24@gmail.com>
2024-05-24 23:43:13 +0200

    introduce type FetchedJourney to server code

--- src/lib/server/treePlantation.ts
+++ src/lib/server/treePlantation.ts
@@ -1,6 +1,6 @@
 import { client } from "$lib/server/setup";
 import type { JourneysOptions } from "hafas-client";
-import type { JourneyNode, TransitType, ZugResponse } from "$lib/types";
+import type { JourneyBlock, JourneyNode, ParsedTime, TransitType, ZugResponse } from "$lib/types";
 import { getSuccessResponse } from "$lib/server/responses";
 import { journeyToBlocks } from "$lib/server/parse";
 import { getFirstAndLastTime } from "$lib/util";
@@ -19,8 +19,15 @@ type TimeLimit = {
 	date: Date;
 };
 
+type FetchedJourney = {
+	refreshToken: string;
+	blocks: JourneyBlock[];
+	arrivalTime: NonNullable<ParsedTime["arrival"]>;
+	departureTime: NonNullable<ParsedTime["departure"]>;
+};
+
 type JourneyNodesWithRefs = {
-	nodes: JourneyNode[];
+	journeys: FetchedJourney[];
 	earlierRef: string;
 	laterRef: string;
 };
@@ -30,15 +37,20 @@ export async function getJourneyTree(
 	opt: JourneysOptions,
 	transitType: TransitType
 ): Promise<ZugResponse<JourneyNode[]>> {
+	opt.routingMode = transitType === "departure" ? "REALTIME" : "HYBRID"; // https://github.com/public-transport/hafas-client/issues/282
+	opt.stopovers = true;
+	opt.polylines = true;
+	opt.startWithWalking = true;
+
 	// find all journeys of the tree
-	let nodess = await getNodeArrays(stops, opt, transitType);
+	let journeyArrays = await getJourneyArrays(stops, opt, transitType);
 
 	// remove nodes that would lack a parent in the resulting tree
-	nodess = cleanupNodeArrays(nodess);
+	journeyArrays = cleanupNodeArrays(journeyArrays);
 
 	progressInDepths = Array.from({ length: stops.length - 1 }, () => 0);
 	// construct the tree
-	const result = getTreeFromNodeArrays(nodess, new Date(MAX_DATE), 0);
+	const result = getTreeFromNodeArrays(journeyArrays, new Date(MAX_DATE), 0);
 
 	return getSuccessResponse<JourneyNode[]>(result);
 }
@@ -49,22 +61,17 @@ export async function getJourneyTree(
  * @param opt user options
  * @param transitType meaning of the time the user specified
  */
-async function getNodeArrays(
+async function getJourneyArrays(
 	stops: string[],
 	opt: JourneysOptions,
 	transitType: TransitType
-): Promise<JourneyNode[][]> {
+): Promise<FetchedJourney[][]> {
 	// if `transitType` is `arrival`, the journeys have to be searched in reverse order
 	const reverseOrder = transitType === "arrival";
 	const complimentaryTransitType = reverseOrder ? "departure" : "arrival";
 	const fallbackDate = new Date(transitType === "departure" ? 0 : MAX_DATE);
 
-	opt.routingMode = reverseOrder ? "HYBRID" : "REALTIME"; // https://github.com/public-transport/hafas-client/issues/282
-	opt.stopovers = true;
-	opt.polylines = true;
-	opt.startWithWalking = true;
-
-	const nodess: JourneyNode[][] = [];
+	const journeyss: FetchedJourney[][] = [];
 	const timeLimit: TimeLimit = {
 		type: transitType,
 		date: fallbackDate
@@ -81,20 +88,18 @@ async function getNodeArrays(
 	for (let i = start; reverseOrder ? i >= 0 : i < stops.length - 1; i += step) {
 		fromToOpt.from = stops[i];
 		fromToOpt.to = stops[i + 1];
-		const depthNodes = await findJourneysUntil({ ...fromToOpt }, timeLimit);
+		const depthJourneys = await findJourneysUntil({ ...fromToOpt }, timeLimit);
 		fromToOpt.opt[transitType] =
-			depthNodes.at(reverseOrder ? -1 : 0)?.[complimentaryTransitType][
-				complimentaryTransitType
-			]?.time ?? fallbackDate;
+			depthJourneys.at(reverseOrder ? -1 : 0)?.[`${complimentaryTransitType}Time`]?.time ??
+			fallbackDate;
 
-		nodess[i] = depthNodes;
+		journeyss[i] = depthJourneys;
 		timeLimit.date =
-			depthNodes.at(reverseOrder ? 0 : -1)?.[complimentaryTransitType][
-				complimentaryTransitType
-			]?.time ?? fallbackDate;
+			depthJourneys.at(reverseOrder ? 0 : -1)?.[`${complimentaryTransitType}Time`]?.time ??
+			fallbackDate;
 	}
 
-	return nodess;
+	return journeyss;
 }
 
 /**
@@ -102,34 +107,37 @@ async function getNodeArrays(
  * @param fromToOpt origin, destination and options
  * @param limit when to stop looking for more journeys
  */
-async function findJourneysUntil(fromToOpt: FromToOpt, limit: TimeLimit): Promise<JourneyNode[]> {
+async function findJourneysUntil(
+	fromToOpt: FromToOpt,
+	limit: TimeLimit
+): Promise<FetchedJourney[]> {
 	fromToOpt.opt = { ...fromToOpt.opt };
-	const nodes: JourneyNode[] = [];
+	const journeys: FetchedJourney[] = [];
 	const timeComparisonPrefix = limit.type === "departure" ? "later" : "earlier";
 
 	// find first journeys
-	let lastNodesWithRefs = await findJourneys(fromToOpt);
-	nodes.push(...lastNodesWithRefs.nodes);
+	let lastJourneysWithRef = await findJourneys(fromToOpt);
+	journeys.push(...lastJourneysWithRef.journeys);
 
 	// find remaining journeys
 	delete fromToOpt.opt[limit.type]; // laterThan/earlierThan will be used instead
 	let remainingSearches = SEARCH_MAX_THRESHOLD;
 	// repeat finding process until limit is exceeded or `SEARCH_MAX_THRESHOLD` times
 	while (
-		!nodesExceedLimit[limit.type](lastNodesWithRefs.nodes, limit.date) &&
+		!journeysExceedLimit[limit.type](lastJourneysWithRef.journeys, limit.date) &&
 		remainingSearches > 0
 	) {
 		fromToOpt.opt[`${timeComparisonPrefix}Than`] =
-			lastNodesWithRefs[`${timeComparisonPrefix}Ref`];
-		lastNodesWithRefs = await findJourneys(fromToOpt);
+			lastJourneysWithRef[`${timeComparisonPrefix}Ref`];
+		lastJourneysWithRef = await findJourneys(fromToOpt);
 		if (limit.type === "departure") {
-			nodes.push(...lastNodesWithRefs.nodes);
+			journeys.push(...lastJourneysWithRef.journeys);
 		} else {
-			nodes.unshift(...lastNodesWithRefs.nodes);
+			journeys.unshift(...lastJourneysWithRef.journeys);
 		}
 		remainingSearches--;
 	}
-	return nodes;
+	return journeys;
 }
 
 /**
@@ -138,12 +146,11 @@ async function findJourneysUntil(fromToOpt: FromToOpt, limit: TimeLimit): Promis
  * - `departure` compares the limit with the first departure of the given nodes
  * - `arrival` compares the limit with the last arrival of the given nodes
  */
-const nodesExceedLimit: { [K in TransitType]: (nodes: JourneyNode[], time: Date) => boolean } = {
-	departure: (nodes, time) =>
-		(nodes[0].departure.departure?.time.getTime() ?? 0) > time.getTime(),
-	arrival: (nodes, time) => {
-		return (nodes[0].arrival.arrival?.time.getTime() ?? MAX_DATE) < time.getTime();
-	}
+const journeysExceedLimit: {
+	[K in TransitType]: (journeys: FetchedJourney[], time: Date) => boolean;
+} = {
+	departure: (journeys, time) => journeys[0].departureTime.time.getTime() > time.getTime(),
+	arrival: (journeys, time) => journeys[0].arrivalTime.time.getTime() < time.getTime()
 };
 
 /**
@@ -153,18 +160,17 @@ const nodesExceedLimit: { [K in TransitType]: (nodes: JourneyNode[], time: Date)
 async function findJourneys(fromToOpt: FromToOpt): Promise<JourneyNodesWithRefs> {
 	return client.journeys(fromToOpt.from, fromToOpt.to, fromToOpt.opt).then((journeys) => {
 		return {
-			nodes:
+			journeys:
 				journeys.journeys
 					?.filter((journey) => journey.legs.every((leg) => !leg.cancelled))
-					.map((journey) => {
+					.map((journey): FetchedJourney => {
 						const blocks = journeyToBlocks(journey);
+						const { arrival, departure } = getFirstAndLastTime(blocks);
 						return {
 							refreshToken: journey.refreshToken ?? "",
 							blocks,
-							children: [],
-							...getFirstAndLastTime(blocks),
-							depth: 0,
-							idInDepth: 0
+							arrivalTime: arrival.arrival ?? { time: new Date(0) },
+							departureTime: departure.departure ?? { time: new Date(0) }
 						};
 					}) ?? [],
 			earlierRef: journeys.earlierRef ?? "",
@@ -175,40 +181,40 @@ async function findJourneys(fromToOpt: FromToOpt): Promise<JourneyNodesWithRefs>
 
 /**
  * removes completely unreachable journeys
- * @param nodess node arrays for each section of the full journey
+ * @param journeyss node arrays for each section of the full journey
  */
-function cleanupNodeArrays(nodess: JourneyNode[][]): JourneyNode[][] {
-	for (let i = 1; i < nodess.length; i++) {
-		const firstArrivalOfLastNodes = nodess[i - 1][0].arrival.arrival?.time.getTime();
+function cleanupNodeArrays(journeyss: FetchedJourney[][]): FetchedJourney[][] {
+	for (let i = 1; i < journeyss.length; i++) {
+		const firstArrivalOfLastNodes = journeyss[i - 1][0].arrivalTime.time.getTime();
 		let indexOfFirstValidNode = 0;
 		// increment this while `nodess[i][indexOfFirstValidNode]` starts before `firstArrivalOfLastNodes`
 		while (
-			indexOfFirstValidNode < nodess[i].length &&
-			(nodess[i][indexOfFirstValidNode].departure.departure?.time.getTime() ?? 0) <
-				(firstArrivalOfLastNodes ?? 0)
+			indexOfFirstValidNode < journeyss[i].length &&
+			journeyss[i][indexOfFirstValidNode].departureTime.time.getTime() <
+				firstArrivalOfLastNodes
 		) {
 			indexOfFirstValidNode++;
 		}
 		// remove nodes starting to early
-		nodess[i] = nodess[i].slice(indexOfFirstValidNode);
+		journeyss[i] = journeyss[i].slice(indexOfFirstValidNode);
 	}
-	return nodess;
+	return journeyss;
 }
 
 let progressInDepths: number[];
 
 /**
  * recursively turn array of journeys into a tree fulfilling the constraints described in the readme
- * @param nodess each element contains the journeys of one subsection of the entire trip
+ * @param journeyss each element contains the journeys of one subsection of the entire trip
  * @param includeUntil every returned journey starts before this time
  * @param depth recursion depth. only `nodess[depth]` will be considered in this recursion step
  */
 function getTreeFromNodeArrays(
-	nodess: JourneyNode[][],
+	journeyss: FetchedJourney[][],
 	includeUntil: Date,
 	depth: number
 ): JourneyNode[] {
-	if (depth === nodess.length) {
+	if (depth === journeyss.length) {
 		// base case
 		return [];
 	}
@@ -217,29 +223,26 @@ function getTreeFromNodeArrays(
 
 	// include nodes until no more nodes are left over or the next journey starts after `includeUntil`
 	while (
-		progressInDepths[depth] < nodess[depth].length &&
-		!nodeExceedsTime(nodess[depth][progressInDepths[depth]], includeUntil)
+		progressInDepths[depth] < journeyss[depth].length &&
+		journeyss[depth][progressInDepths[depth]].departureTime.time.getTime() <
+			includeUntil.getTime()
 	) {
 		const nextArrival =
-			nodess[depth].at(progressInDepths[depth] + 1)?.arrival.arrival?.time ??
+			journeyss[depth].at(progressInDepths[depth] + 1)?.arrivalTime?.time ??
 			new Date(MAX_DATE);
 
-		const nodeToBeIncluded = nodess[depth][progressInDepths[depth]];
-		nodeToBeIncluded.children = getTreeFromNodeArrays(nodess, nextArrival, depth + 1);
-		nodeToBeIncluded.depth = depth;
-		nodeToBeIncluded.idInDepth = progressInDepths[depth];
-
-		nodesToBeIncluded.push(nodess[depth][progressInDepths[depth]]);
+		const nodeJourneys = journeyss[depth][progressInDepths[depth]];
+		const nodeToBeIncluded: JourneyNode = {
+			blocks: nodeJourneys.blocks,
+			children: getTreeFromNodeArrays(journeyss, nextArrival, depth + 1),
+			refreshToken: nodeJourneys.refreshToken,
+			depth,
+			idInDepth: progressInDepths[depth],
+			arrival: { arrival: nodeJourneys.arrivalTime },
+			departure: { departure: nodeJourneys.departureTime }
+		};
+		nodesToBeIncluded.push(nodeToBeIncluded);
 		progressInDepths[depth]++;
 	}
 	return nodesToBeIncluded;
-}
-
-/**
- * check if a node begins after the given time
- * @param node a node holding a journey
- * @param time the time to compare against
- */
-function nodeExceedsTime(node: JourneyNode, time: Date): boolean {
-	return (node.departure.departure?.time.getTime() ?? 0) >= time.getTime();
 }
