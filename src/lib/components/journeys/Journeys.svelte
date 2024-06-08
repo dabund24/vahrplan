eb41d428636f32b593a139182b18e34ed4cc9eea
dabund24 <dabund24@gmail.com>
2024-06-08 15:35:59 +0200

    add warning to impossible journeys

--- src/lib/components/journeys/Journeys.svelte
+++ src/lib/components/journeys/Journeys.svelte
@@ -7,12 +7,48 @@
 	import { displayedJourneys, selectedJourneys } from "$lib/stores/journeyStores";
 	import DateDuration from "$lib/components/DateDuration.svelte";
 	import { dateDifference } from "$lib/util";
+	import type { JourneyBlock, LegBlock, TransferBlock, WalkingBlock } from "$lib/types";
+	import Warning from "$lib/components/Warning.svelte";
 
 	$: selectedDeparture = $selectedJourneys.at(0)?.departure.departure?.time;
 	$: selectedArrival = $selectedJourneys.at(-1)?.arrival.arrival?.time;
+
+	$: flatBlocks = $displayedJourneys.flatMap((journey) => journey.value);
+
+	$: hasImpossibleTransfer = flatBlocks
+		.filter(isTransferOrWalkingBlock)
+		.some((transfer) => transfer.transferTime < 0);
+
+	function isTransferOrWalkingBlock(block: JourneyBlock): block is (TransferBlock | WalkingBlock) {
+		return block.type === "transfer" || block.type === "walk";
+	}
+
+	$: hasCancelledLeg = flatBlocks.filter(isLegBlock).some((leg) => leg.attribute === "cancelled");
+
+	function isLegBlock(block: JourneyBlock): block is LegBlock {
+		return block.type === "leg";
+	}
+
+	$: warningMessage = getWarningMessage(hasImpossibleTransfer, hasCancelledLeg);
+
+	function getWarningMessage(
+		hasImpossibleTransfer: boolean,
+		hasCancelledLeg: boolean
+	): string | undefined {
+		if (hasImpossibleTransfer && hasCancelledLeg) {
+			return "Reise nicht möglich: Negative Umstiegszeit und Fahrt entfällt";
+		} else if (hasImpossibleTransfer) {
+			return "Reise nicht möglich: Negative Umstiegszeit";
+		} else if (hasCancelledLeg) {
+			return "Reise nicht möglich: Fahrt entfällt";
+		}
+	}
 </script>
 
 <div class="journeys-container">
+	{#if warningMessage !== undefined}
+		<Warning message={warningMessage} color="red" />
+	{/if}
 	<DateDuration
 		date={selectedDeparture}
 		duration={dateDifference(selectedDeparture, selectedArrival)}
