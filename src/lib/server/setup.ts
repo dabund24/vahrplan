61fe63473a650fae24a1767bbcc6dd6d6c9da6d7
dabund24 <dabund24@gmail.com>
2024-07-12 12:28:52 +0200

    replace occurrences of zug with vahrplan

--- src/lib/server/setup.ts
+++ src/lib/server/setup.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'hafas-client';
-import { profile as dbProfile } from 'hafas-client/p/db/index.js';
+import { createClient } from "hafas-client";
+import { profile as dbProfile } from "hafas-client/p/db/index.js";
 
-const userAgent = 'github.com/dabund24/Zug';
+const userAgent = "github.com/dabund24/vahrplan";
 
 export const client = createClient(dbProfile, userAgent);
