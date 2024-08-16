b8064676b3862379e3bcbc5d9da0114778d931b1
dabund24 <dabund24@gmail.com>
2024-08-16 12:11:53 +0200

    change details icon in navbar

--- src/lib/components/icons/IconDetails.svelte
+++ src/lib/components/icons/IconDetails.svelte
@@ -1,10 +1,14 @@
 <svg width="26px" height="26px" xmlns="http://www.w3.org/2000/svg">
 	<title>Verbindungsdetails</title>
 	<g stroke="var(--foreground-color)" stroke-width="3" fill="none">
-		<polyline points="6,6 22,6 4,20 20,20" stroke-linejoin="round" />
+		<line x1="5" y1="5" x2="17" y2="5" />
+		<line x1="9" y1="13" x2="17" y2="13" />
+		<line x1="9" y1="21" x2="21" y2="21" />
+		<path d="M 17 5 A 4 4 0 0 1 17 13" />
+		<path d="M 9 13 A 4 4 0 0 0 9 21" />
 	</g>
-	<g stroke="var(--accent-color)" stroke-width="3" fill="var(--background-color)">
-		<circle r="4.5" cx="6" cy="6" />
-		<circle r="4.5" cx="20" cy="20" />
+	<g stroke="var(--accent-color)" stroke-width="2.5" fill="var(--background-color)">
+		<circle r="3.5" cx="5" cy="5" />
+		<circle r="3.5" cx="21" cy="21" />
 	</g>
 </svg>
