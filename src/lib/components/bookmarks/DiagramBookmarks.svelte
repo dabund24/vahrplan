f506e9bc09ae25823e2122548a17bb96e53654cc
dabund24 <dabund24@gmail.com>
2024-08-22 18:35:50 +0200

    add options to diagram bookmarks in bookmarks sub-page

--- src/lib/components/bookmarks/DiagramBookmarks.svelte
+++ src/lib/components/bookmarks/DiagramBookmarks.svelte
@@ -1,46 +1,103 @@
 <script lang="ts">
-	import { type DiagramBookmark, getBookmarks } from "$lib/bookmarks";
+	import { type DiagramBookmark, getBookmarks, setBookmarks } from "$lib/bookmarks";
 	import { dateToString, timeToString } from "$lib/util";
 	import IconStationLocation from "$lib/components/icons/IconStationLocation.svelte";
-	import { onMount } from "svelte";
+	import { type ComponentProps, onMount } from "svelte";
+	import Options from "$lib/components/Options.svelte";
+	import IconClose from "$lib/components/icons/IconClose.svelte";
+	import { goto } from "$app/navigation";
+	import IconRightArrow from "$lib/components/icons/IconRightArrow.svelte";
+	import { scale } from "svelte/transition";
+	import { flip } from "svelte/animate";
 
-	let bookmarks: DiagramBookmark[] = [];
+	let bookmarks: DiagramBookmark[] = $state([]);
 
 	onMount(() => {
-		bookmarks = getBookmarks("diagram")
-	})
+		bookmarks = getBookmarks("diagram");
+	});
+
+	function getOptions(bookmarkIndex: number): ComponentProps<Options>["options"] {
+		return [
+			{
+				name: "Diagramm anzeigen",
+				onClick: () => goto(new URL(bookmarks[bookmarkIndex].link)),
+				icon: iconRightArrow
+			},
+			{
+				name: "Lesezeichen löschen",
+				onClick: () => {
+					bookmarks.splice(bookmarkIndex, 1);
+					setBookmarks({ type: "diagram", bookmarks });
+				},
+				icon: iconClose
+			}
+		];
+	}
 </script>
 
-<ol>
-	{#each bookmarks as bookmark}
-		<li class="padded-top-bottom">
-			<a href={bookmark.link} class="hoverable button--small flex-column">
-				<div class="padded-top-bottom">{bookmark.transitType === "departure" ? "Abfahrt" : "Ankunft"}: {dateToString(bookmark.time)}, {timeToString(bookmark.time)}</div>
-				<ol>
-					{#each bookmark.stops as stop}
-						<li class="flex-row padded-top-bottom stop">
-							<div class="icon flex-column">
-								<IconStationLocation color={"foreground"} iconType={stop.type} />
-							</div>
-							<strong class="limit-lines">
-								{stop.name}
-							</strong>
-						</li>
-					{/each}
-				</ol>
-			</a>
-		</li>
-	{/each}
-</ol>
+{#snippet iconClose()}
+	<IconClose />
+{/snippet}
+{#snippet iconRightArrow()}
+	<IconRightArrow />
+{/snippet}
+
+{#if bookmarks.length > 0}
+	<ol class="flex-column bookmarks">
+		{#each bookmarks as bookmark, i (bookmark.link)}
+			<li class="flex-row" transition:scale animate:flip={{ duration: 400 }}>
+				<a href={bookmark.link} class="hoverable button--small flex-column">
+					<div class="time-data">
+						{bookmark.transitType === "departure" ? "Abfahrt" : "Ankunft"}: {dateToString(
+							bookmark.time
+						)}, {timeToString(bookmark.time)}
+					</div>
+					<ol>
+						{#each bookmark.stops as stop}
+							<li class="flex-row padded-top-bottom stop">
+								<div class="icon flex-column">
+									<IconStationLocation
+										color={"foreground"}
+										iconType={stop.type}
+									/>
+								</div>
+								<strong class="limit-lines">
+									{stop.name}
+								</strong>
+							</li>
+						{/each}
+					</ol>
+				</a>
+				<div class="options-container">
+					<Options id={`diagram-bookmark__${i}`} options={getOptions(i)} />
+				</div>
+			</li>
+		{/each}
+	</ol>
+{/if}
 
 <style>
+	.bookmarks {
+		gap: 0.5rem;
+	}
+
 	a {
 		text-decoration: none;
-		padding: .5rem 1rem;
+		padding: 0 1rem;
+		width: 100%;
+		margin-right: calc(-24px - 1rem);
+	}
+
+	.options-container {
+		height: fit-content;
+	}
+
+	.time-data {
+		padding: 0.5rem 2rem 0.5rem 0;
 	}
 
 	strong {
-		margin-left: .5rem;
+		margin-left: 0.5rem;
 		-webkit-line-clamp: 3;
 	}
 
@@ -50,27 +107,24 @@
 
 	.icon::before,
 	.icon::after {
-        content: "";
-        width: 4px;
-        height: 2.5rem;
-        background-color: var(--foreground-color);
-    }
+		content: "";
+		width: 4px;
+		height: 2.5rem;
+		background-color: var(--foreground-color);
+	}
 
 	.stop:first-child .icon::before {
 		background-color: transparent;
 	}
 
-    .stop:last-child .icon::after {
-        background-color: transparent;
-    }
+	.stop:last-child .icon::after {
+		background-color: transparent;
+	}
 
 	.icon::before {
 		margin: -2rem auto -8px;
 	}
-    .icon::after {
-        margin: -8px auto -2rem;
-    }
-
-
-
+	.icon::after {
+		margin: -8px auto -2rem;
+	}
 </style>
---
+++ src/lib/components/icons/IconRightArrow.svelte
@@ -0,0 +1,6 @@
+<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+	<g stroke="var(--foreground-color)" stroke-linecap="round" stroke-width="3">
+		<line x1="2" y1="8" x2="14" y2="8" />
+		<polyline points="8,2 14,8 8,14" stroke-linejoin="round" fill="none" />
+	</g>
+</svg>
