b039c317b4f6e235b66ec2a50a9366508564b1dc
dabund24 <dabund24@gmail.com>
2024-08-26 13:34:56 +0200

    add icons for tabs in journey subpage

---
+++ src/lib/components/icons/IconMap.svelte
@@ -0,0 +1,7 @@
+<svg width='16px' height='16px' xmlns='http://www.w3.org/2000/svg'>
+	<g stroke-width="3" stroke="var(--foreground-color)" stroke-linecap="round" fill="none">
+		<path d="M 4 10 A 5 5 0 1 1 12 10" fill="none" />
+		<polyline points="4,10 8,14 12,10" fill="none" stroke-linejoin="round" />
+	</g>
+	<circle cx="8" cy="7" r="2" fill="var(--foreground-color)" />
+</svg>
