98248a8ec43d48d308229570d7ef75eda04abd9c
dabund24 <dabund24@gmail.com>
2024-08-29 18:45:43 +0200

    add component titleless header

---
+++ src/lib/components/TitlelessHeader.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Props = {
+		isBelowHeaderMobile?: boolean;
+		isBelowHeaderDesktop?: boolean;
+		children: Snippet;
+	};
+
+	let { isBelowHeaderMobile = false, isBelowHeaderDesktop = false, children }: Props = $props();
+</script>
+
+<div
+	class="titleless-header"
+	class:below-header--mobile={isBelowHeaderMobile}
+	class:below-header--desktop={isBelowHeaderDesktop}
+>
+	{@render children()}
+	<div class="transition"></div>
+</div>
+
+<style>
+	.titleless-header {
+		width: 100%;
+		position: sticky;
+		top: 0;
+        z-index: 500;
+		& > :global(:not(:last-child)) {
+			background-color: var(--background-color--transparent);
+		}
+	}
+
+    .transition {
+        --background-color--transparent--transitionable: var(--background-color--transparent);
+        background: linear-gradient(
+                to bottom,
+                var(--background-color--transparent--transitionable),
+                transparent
+        );
+        transition: --background-color--transparent--transitionable 0.4s var(--cubic-bezier);
+    }
+
+    @property --background-color--transparent--transitionable {
+        syntax: "<color>";
+        initial-value: #ffffffe0;
+        inherits: false;
+    }
+
+    @media screen and (min-width: 1000px) {
+        .below-header--desktop {
+            top: calc(3.5rem + 4px);
+        }
+    }
+
+    @media screen and (max-width: 999px) {
+        .below-header--mobile {
+            top: calc(3.5rem + 4px);
+        }
+    }
+</style>
