d14ef28b4a6045ca77c59e8a10a051cf72dfc837
dabund24 <dabund24@gmail.com>
2024-08-29 16:25:02 +0200

    add minitabs component

---
+++ src/lib/components/MiniTabs.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { ComponentProps, Snippet } from "svelte";
+	import SingleSelect from "$lib/components/SingleSelect.svelte";
+
+	type Props = {
+		padContent?: boolean;
+		tabs: {
+			title: string; // for accessibility
+			icon: Snippet; // the icon used
+			content: Snippet; // the content associated with the tab
+		}[];
+		tabEnvironment: Snippet<[Snippet]>;
+	};
+
+	const { padContent = false, tabs, tabEnvironment } = $props() as Props;
+	const tabsWithType: ComponentProps<SingleSelect<"icon">>["titles"] = tabs.map((tab) => ({
+		title: tab.title,
+		icon: tab.icon,
+		type: "icon"
+	}));
+
+	let activeTab = $state(0);
+</script>
+
+{#snippet miniTabs()}
+	<SingleSelect titles={tabsWithType} bind:selected={activeTab} />
+{/snippet}
+
+{@render tabEnvironment(miniTabs)}
+<div class:pad-content={padContent}>
+	{@render tabs[activeTab].content()}
+</div>
+
+<style>
+	.pad-content {
+		padding: 0 1rem;
+	}
+</style>
