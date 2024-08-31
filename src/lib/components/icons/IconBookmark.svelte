b4cb3d871b8f870109077c22ae1c6faada735867
dabund24 <dabund24@gmail.com>
2024-08-31 16:32:48 +0200

    adjust size of small bookmark icon to align its style with similar icons

--- src/lib/components/icons/IconBookmark.svelte
+++ src/lib/components/icons/IconBookmark.svelte
@@ -2,13 +2,26 @@
 	export let isBookmarked: boolean;
 </script>
 
-<svg class:bookmarked={isBookmarked} width="16px" height="16px" xmlns="http://www.w3.org/2000/svg">
-	<polyline points="4,2 12,2 12,14 8,10 4,14 4,2" stroke-width="2.5" stroke="var(--foreground-color)" stroke-linejoin="round" stroke-linecap="round" fill="transparent" />
+<svg
+	class:bookmarked={isBookmarked}
+	width="16px"
+	height="16px"
+	viewBox="0 0 20 20"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<polyline
+		points="5,2 15,2 15,18 10,13 5,18 5,2"
+		stroke-width="3"
+		stroke="var(--foreground-color)"
+		stroke-linejoin="round"
+		stroke-linecap="round"
+		fill="transparent"
+	/>
 </svg>
 
 <style>
 	.bookmarked polyline {
-        fill: var(--accent-color);
-        stroke: var(--accent-color);
+		fill: var(--accent-color);
+		stroke: var(--accent-color);
 	}
 </style>
