85d86b4ff5255c919b960c40b6662a434c6fde1d
dabund24 <dabund24@gmail.com>
2024-08-31 15:39:46 +0200

    add accordion element component

---
+++ src/lib/components/AccordionElement.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Props = {
+		title: string;
+		children: Snippet;
+	};
+
+	let { title, children }: Props = $props();
+</script>
+
+<details class="padded-top-bottom">
+	<summary class="flex-row hoverable hoverable--visible">
+		{title}
+		<svg width="16px" height="16px" xmlns="http://www.w3.org/2000/svg">
+			<g stroke="var(--foreground-color)" stroke-width="3" fill="none" stroke-linecap="round">
+				<line x1="8" y1="11" x2="4" y2="7" />
+				<line x1="8" y1="11" x2="12" y2="7" />
+			</g>
+		</svg>
+	</summary>
+	{@render children()}
+</details>
+
+<style>
+	details[open] svg {
+		transform: scaleY(-1);
+	}
+	summary {
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 0.5rem;
+	}
+</style>
