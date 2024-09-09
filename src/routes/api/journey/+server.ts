bb04f53802d913478e9c8aaf4c45883992ff3bc2
dabund24 <dabund24@gmail.com>
2024-09-09 14:09:36 +0200

    improve error handling for api journey endpoint

--- src/routes/api/journey/+server.ts
+++ src/routes/api/journey/+server.ts
@@ -1,8 +1,9 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { hafasClient } from "$lib/server/setup";
 import { journeyToBlocks } from "$lib/server/parse/parse";
-import { getSuccessResponse, getZugError } from "$lib/server/responses";
-import type { JourneyBlock, UnselectedBlock, ZugResponse } from "$lib/types";
+import { getSuccessResponse, getZugError, getZugErrorFromHafasError } from "$lib/server/responses";
+import type { JourneyBlock, ZugResponse } from "$lib/types";
+import type { Journey } from "hafas-client";
 
 export const GET: RequestHandler = async function ({ url }) {
 	const hafasTokens = getHafasTokensFromUrl(url);
@@ -18,10 +19,10 @@ export const GET: RequestHandler = async function ({ url }) {
  * @param url should either have `j` param for short token (resolves at database to hafas-tokens)
  * or `tokens` param containing the tokens directly
  */
-function getHafasTokensFromUrl(url: URL): (string | null)[] | undefined {
+function getHafasTokensFromUrl(url: URL): string[] | undefined {
 	const tokenParam = decodeURIComponent(url.searchParams.get("tokens") ?? "");
 	try {
-		return JSON.parse(tokenParam) as (string | null)[];
+		return JSON.parse(tokenParam) as string[];
 	} catch {
 		return undefined;
 	}
@@ -32,25 +33,24 @@ function getHafasTokensFromUrl(url: URL): (string | null)[] | undefined {
  * @param tokens refresh tokens as defined in hafas. `null` will return an unselected block
  * @returns a journey as an array of sub-journeys where a sub-journey consists of multiple journey blocks
  */
-async function getJourneyByTokens(
-	tokens: (string | null)[]
-): Promise<ZugResponse<JourneyBlock[][]>> {
-	const journeys = await Promise.all(
-		tokens.map(async (token): Promise<JourneyBlock[]> => {
-			if (token === null) {
-				return [{ type: "unselected" }] as UnselectedBlock[];
-			}
-			return (
-				hafasClient
-					.refreshJourney?.(token, {
-						stopovers: true,
-						language: "de",
-						polylines: true
-					})
-					.then((journeyWRD) => journeyToBlocks(journeyWRD.journey))
-					.catch(() => [{ type: "error" }]) ?? [{ type: "error" }]
-			);
-		})
-	);
-	return getSuccessResponse(journeys);
+async function getJourneyByTokens(tokens: string[]): Promise<ZugResponse<JourneyBlock[][]>> {
+	try {
+		const hafasJourneys = await Promise.all(
+			tokens.map(async (token): Promise<Journey> => {
+				return (
+					hafasClient
+						.refreshJourney?.(token, {
+							stopovers: true,
+							language: "de",
+							polylines: true
+						})
+						.then((journeyWRD) => journeyWRD.journey) ?? { type: "journey", legs: [] }
+				);
+			})
+		);
+		const blocks = hafasJourneys.map(journeyToBlocks);
+		return getSuccessResponse(blocks);
+	} catch (e) {
+		return getZugErrorFromHafasError(e);
+	}
 }
