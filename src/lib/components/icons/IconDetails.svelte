2bd1bebceba9534bbc111be5bd5941fe32ff5d99
dabund24 <dabund24@gmail.com>
2024-09-11 17:46:12 +0200

    remove tiny gaps for details icon

--- src/lib/components/icons/IconDetails.svelte
+++ src/lib/components/icons/IconDetails.svelte
@@ -1,6 +1,6 @@
 <svg width="26px" height="26px" xmlns="http://www.w3.org/2000/svg">
 	<title>Verbindungsdetails</title>
-	<g stroke="var(--foreground-color)" stroke-width="3" fill="none">
+	<g stroke="var(--foreground-color)" stroke-width="3" fill="none" stroke-linecap="round">
 		<line x1="5" y1="5" x2="17" y2="5" />
 		<line x1="9" y1="13" x2="17" y2="13" />
 		<line x1="9" y1="21" x2="21" y2="21" />
