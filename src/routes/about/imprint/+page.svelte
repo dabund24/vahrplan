47ec79176dcc7e48c1609a9ee25378b168eb3813
dabund24 <dabund24@gmail.com>
2024-09-13 23:23:50 +0200

    add imprint

---
+++ src/routes/about/imprint/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import {
+		PUBLIC_IMPRINT_TOWN,
+		PUBLIC_IMPRINT_STREET,
+		PUBLIC_IMPRINT_EMAIL_ADDRESS,
+		PUBLIC_PHONE_NUMBER
+	} from "$env/static/public";
+</script>
+
+<svelte:head>
+	<title>Vahrplan - Impressum</title>
+	<meta name="title" content="Vahrplan - Impressum" />
+	<meta name="description" content="Das Impressum von Vahrplan" />
+</svelte:head>
+
+<div class="content-wrapper">
+	<h1>Impressum</h1>
+	<p>
+		Daniel Bund<br />
+		{PUBLIC_IMPRINT_STREET}<br />
+		{PUBLIC_IMPRINT_TOWN}<br />
+		Deutschland
+	</p>
+	<p>
+		Kontakt:<br />
+		E-Mail: {PUBLIC_IMPRINT_EMAIL_ADDRESS}<br />
+		Telefon: {PUBLIC_PHONE_NUMBER}
+	</p>
+</div>
+
+<style>
+	.content-wrapper {
+		padding: 0.5rem 1rem;
+	}
+</style>
