e81a58eb1a882d8ee63d2962197520597852cfd3
dabund24 <dabund24@gmail.com>
2024-09-20 13:37:02 +0200

    make error page look a little nicer

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,17 @@
 </script>
 
 <div class="content-wrapper">
-	<h1>Fehler {$page.status} :(</h1>
+	<h1>Fehler {$page.status}</h1>
+	<div class="line--regular line--accent"></div>
 	<p>{$page.error?.message}</p>
 </div>
+
+<style>
+	.content-wrapper {
+		padding: .5rem 1rem;
+	}
+
+	.line--accent {
+		width: var(--line-length);
+	}
+</style>
