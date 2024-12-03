60e2bd618de2491a91fea6b29ea74bb97b7e11c0
dabund24 <83349242+dabund24@users.noreply.github.com>
2024-12-03 11:36:00 +0100

    Cache successes only (#68)
    * don't cache error responses
    
    * eliminate some magic numbers in server hooks
    
    * dynamically determine current year
    
    * write cache function in service worker a little more concise
    
    * update to version 2.3.1

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,39 +2,38 @@ import { sequence } from "@sveltejs/kit/hooks";
 import { type Handle, json } from "@sveltejs/kit";
 import { RateLimiter } from "$lib/server/RateLimiter";
 
+const CACHE_DURATION_SECONDS = new Map<string, number>();
+const YEAR_IN_SECONDS = 31536000
+CACHE_DURATION_SECONDS.set("/api/diagram", 120);
+CACHE_DURATION_SECONDS.set("/diagram", 120);
+CACHE_DURATION_SECONDS.set("/api/journey", 30);
+CACHE_DURATION_SECONDS.set("/journey", 30);
+CACHE_DURATION_SECONDS.set("/api/location", YEAR_IN_SECONDS);
+CACHE_DURATION_SECONDS.set("/api/locations", YEAR_IN_SECONDS);
+
+/**
+ * sets the cache-control header of the response based on the url
+ */
+// eslint-disable-next-line @typescript-eslint/unbound-method
 const cache: Handle = async function ({ event, resolve }) {
 	const response = await resolve(event);
-	if (response.status === 429) {
-		// don't cache rate limit responses
+	if (!response.ok) {
+		// don't cache error responses
 		return response;
 	}
-	const cacheDuration = getCacheDuration(event.url);
+	const cacheDuration = CACHE_DURATION_SECONDS.get(event.url.pathname);
 	response.headers.set("Cache-Control", `max-age=${cacheDuration}`);
 	return response;
 };
 
-function getCacheDuration(url: URL): number {
-	switch (url.pathname) {
-		case "/api/diagram":
-		case "/diagram":
-			return 120;
-		case "/api/journey":
-		case "/journey":
-			return 30;
-		case "/api/location":
-			return 31536000;
-		case "/api/locations":
-			return 31536000;
-		default:
-			return 0;
-	}
-}
-
-const userRateLimiter = new RateLimiter(60, 40);
+const rateLimiterIntervalSeconds = 60;
+const rateLimiterAccessThreshold = 40;
+const userRateLimiter = new RateLimiter(rateLimiterIntervalSeconds, rateLimiterAccessThreshold);
 
 /**
  * limits api access for each user
  */
+// eslint-disable-next-line @typescript-eslint/unbound-method
 const userRateLimiting: Handle = function ({ event, resolve }) {
 	if (!event.url.pathname.startsWith("/api/")) {
 		return resolve(event);
