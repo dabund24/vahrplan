7ec22eeb6f5a828cf55b5be0b1eaca53db5871fb
dabund24 <dabund24@gmail.com>
2024-12-25 23:02:23 +0100

    finish ticket icon

--- src/lib/components/icons/IconTickets.svelte
+++ src/lib/components/icons/IconTickets.svelte
@@ -1,12 +1,13 @@
-<svg width="16px" height="16px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+<svg width="16px" height="16px" xmlns="http://www.w3.org/2000/svg">
 	<g
-		stroke-width="3"
-		stroke="var(--foreground-color)"
-		stroke-linejoin="round"
-		stroke-linecap="round"
+			stroke-width="2"
+			stroke="var(--foreground-color)"
+			stroke-linejoin="round"
+			stroke-linecap="round"
+			fill="none"
 	>
-		<line x1="2" y1="8" x2="12" y2="8" />
-		<line x1="2" y1="12" x2="12" y2="12" />
-		<line x1="4" y1="8" x2="4" y2="12" />
+		<line x1="2" y1="6.5" x2="8" y2="6.5" />
+		<line x1="2" y1="9.5" x2="8" y2="9.5" />
+		<path d="M 14 4.5 A 5.5 6 0 1 0 14 11.4"/>
 	</g>
 </svg>
