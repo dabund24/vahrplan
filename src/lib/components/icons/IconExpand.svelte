524f94a032de93c63e5c04956ea7e6a8be84b3c7
dabund24 <dabund24@gmail.com>
2024-12-31 16:01:31 +0100

    fix formatting of expand icon component code

--- src/lib/components/icons/IconExpand.svelte
+++ src/lib/components/icons/IconExpand.svelte
@@ -1,10 +1,5 @@
 <svg width="1rem" height="1rem" viewBox="0 0 16 16">
-	<g
-		stroke="var(--foreground-color)"
-		stroke-width="2"
-		fill="none"
-		stroke-linecap="round"
-	>
+	<g stroke="var(--foreground-color)" stroke-width="2" fill="none" stroke-linecap="round">
 		<line class="expand-line--left" x1="8" y1="11" x2="2" y2="5" />
 		<line class="expand-line--right" x1="8" y1="11" x2="14" y2="5" />
 	</g>
