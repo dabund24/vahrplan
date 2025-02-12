85b62bbafec8796b006ae5dece98aa3cd98761f1
dabund24 <83349242+dabund24@users.noreply.github.com>
2025-02-12 01:11:28 +0100

    Fix dropdown arrow size (#97)
    * fix arrow sizes of dropdowns
    
    * update to version 3.0.2

--- src/lib/components/Setting.svelte
+++ src/lib/components/Setting.svelte
@@ -103,7 +103,7 @@
 
 	select {
 		background: url('data:image/svg+xml;charset=UTF-8,<svg width="1rem" height="1rem" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g stroke="black" stroke-width="2" fill="none" stroke-linecap="round" > <line class="expand-line--left" x1="8" y1="11" x2="2" y2="5" /><line class="expand-line--right" x1="8" y1="11" x2="14" y2="5" /></g></svg>')
-			var(--foreground-color--very-transparent) no-repeat calc(100% - 0.5rem) 50%;
+			var(--foreground-color--very-transparent) no-repeat calc(100% - 0.5rem) 50% / 1rem;
 		-moz-appearance: none;
 		-webkit-appearance: none;
 		appearance: none;
