61198800d5ae2303ee6188c0bca3b1d22417770a
dabund24 <dev@dabund24.de>
2025-05-12 00:09:18 +0200

    fix faulty line shapes script

--- scripts/line-shapes.sh
+++ scripts/line-shapes.sh
@@ -4,4 +4,3 @@ echo "fetching line shapes..."
 curl -L \
      --url 'https://raw.githubusercontent.com/Traewelling/line-colors/refs/heads/main/line-colors.csv' \
      --create-dirs -o './assets/line-shapes.csv'
-ls /app/assets/
