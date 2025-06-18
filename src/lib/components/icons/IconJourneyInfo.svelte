6e4af33ed368352de5369c393408c439d73804dd
dabund24 <dev@dabund24.de>
2025-06-19 00:44:11 +0200

    change journey info icon

--- src/lib/components/icons/IconJourneyInfo.svelte
+++ src/lib/components/icons/IconJourneyInfo.svelte
@@ -1,7 +1,14 @@
 <svg width="1rem" height="1rem" viewBox="0 0 16 16">
-	<g stroke-width="2" stroke="var(--foreground-color)" stroke-linecap="round">
-		<line x1="2" y1="3" x2="14" y2="3" />
-		<line x1="2" y1="8" x2="14" y2="8" />
-		<line x1="2" y1="13" x2="14" y2="13" />
+	<g
+		stroke-width="2"
+		stroke="var(--foreground-color)"
+		stroke-linecap="round"
+		fill="var(--background-color)"
+	>
+		<line x1="3" y1="3" x2="3" y2="13" />
+		<line x1="9" y1="3" x2="14" y2="3" />
+		<line x1="9" y1="13" x2="14" y2="13" />
+		<circle r="2" cx="3" cy="3" stroke-width="1.5" />
+		<circle r="2" cx="3" cy="13" stroke-width="1.5" />
 	</g>
 </svg>
