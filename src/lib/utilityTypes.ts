7341e4b2a4750406fdd91c4ca3bab63e0fd91564
dabund24 <dev@dabund24.de>
2025-10-05 23:34:39 +0200

    simplify `Includes` utility type

--- src/lib/utilityTypes.ts
+++ src/lib/utilityTypes.ts
@@ -3,16 +3,14 @@ export type CamelToKebab<T extends string, A extends string = ""> = T extends `$
 	: A;
 
 type Includes<T extends readonly unknown[], U> = T extends [infer Head, ...infer Tail]
-	? [U] extends [Head]
+	? U extends Head
 		? true
 		: Includes<Tail, U>
 	: false;
-
 type IsTupleUnique<T extends readonly unknown[]> = T extends [infer Head, ...infer Tail]
 	? Includes<Tail, Head> extends true
 		? false
 		: IsTupleUnique<Tail>
 	: true;
-
 export type AssertUniqueTuple<T extends readonly unknown[]> =
 	IsTupleUnique<T> extends true ? T : never;
