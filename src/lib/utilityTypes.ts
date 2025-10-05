9f45c5ddfe383fa8852df418710a5886fda7f0d1
dabund24 <dev@dabund24.de>
2025-10-05 23:04:50 +0200

    make options property of profiles more light-weight

--- src/lib/utilityTypes.ts
+++ src/lib/utilityTypes.ts
@@ -1,3 +1,18 @@
 export type CamelToKebab<T extends string, A extends string = ""> = T extends `${infer F}${infer R}`
 	? CamelToKebab<R, `${A}${F extends Lowercase<F> ? "" : "-"}${Lowercase<F>}`>
 	: A;
+
+type Includes<T extends readonly unknown[], U> = T extends [infer Head, ...infer Tail]
+	? [U] extends [Head]
+		? true
+		: Includes<Tail, U>
+	: false;
+
+type IsTupleUnique<T extends readonly unknown[]> = T extends [infer Head, ...infer Tail]
+	? Includes<Tail, Head> extends true
+		? false
+		: IsTupleUnique<Tail>
+	: true;
+
+export type AssertUniqueTuple<T extends readonly unknown[]> =
+	IsTupleUnique<T> extends true ? T : never;
--- src/routes/[lang]/[profile]/api/profile/profile.server.ts
+++ src/routes/[lang]/[profile]/api/profile/profile.server.ts
@@ -1,4 +1,5 @@
 import type { Product } from "$lib/types";
+import type { AssertUniqueTuple } from "$lib/utilityTypes";
 
 export type ProfileId = "dbnav";
 export type Language = "de";
@@ -12,21 +13,21 @@ type OptionConfig = {
 };
 
 /**
- * all possible options. A profile uses a subset of those
+ * all options ever available. A profile uses a subset of those
  */
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-interface Options extends Record<string, OptionConfig> {
-	bike: { defaultValue: false; name: { de: "Fahrradmitnahme" } };
-	accessible: { defaultValue: false; name: { de: "Barrierefreies Reisen" } };
-	maxTransfers: { defaultValue: -1; name: { de: "Maximale Umstiegsanzahl" } };
-	minTransferTime: { defaultValue: 0; name: { de: "Mindestumsteigezeit" } };
-}
+const options = {
+	bike: { defaultValue: false, name: { de: "Fahrradmitnahme" } },
+	accessible: { defaultValue: false, name: { de: "Barrierefreies Reisen" } },
+	maxTransfers: { defaultValue: -1, name: { de: "Maximale Umstiegsanzahl" } },
+	minTransferTime: { defaultValue: 0, name: { de: "Mindestumsteigezeit" } }
+} as const satisfies Record<string, OptionConfig>;
+type Options = typeof options;
 
 type ProductConfig = {
-	name: LocaleString;
+	name: LocaleString | string;
 };
 
-type NameWithKnownLocale<T extends { name: Record<Language, string> }> = Omit<T, "name"> & {
+type NameWithKnownLocale<T extends { name: LocaleString | string }> = Omit<T, "name"> & {
 	name: string;
 };
 
@@ -46,17 +47,17 @@ type ProfileConfig<
 /**
  * extend to define a profile
  */
-abstract class Profile<
+export abstract class Profile<
 	IdT extends ProfileId,
 	ProductT extends Product,
-	OptionT extends keyof Options
+	OptionT extends readonly (keyof Options)[]
 > {
 	/** very short identifier used for urls */
 	protected abstract readonly id: IdT;
 	/** human-readable, unique profile name; ideally the city or region where it can be used */
 	protected abstract readonly name: LocaleString;
 	protected abstract readonly products: Record<ProductT, ProductConfig>;
-	protected abstract readonly options: { [K in OptionT]: Options[K] };
+	protected abstract readonly options: AssertUniqueTuple<OptionT>;
 
 	/**
 	 * fix the languages for all features of a feature set
@@ -64,24 +65,39 @@ abstract class Profile<
 	 * @param obj an object with properties, where their values all have a name property mapping languages to names
 	 * @private
 	 */
-	private assignLangNames<T extends Record<string, { name: LocaleString }>>(
+	private assignLangNames<T extends Record<string, { name: LocaleString | string }>>(
 		lang: Language,
 		obj: T
 	): { [K in keyof T]: NameWithKnownLocale<T[K]> } {
 		const res = {} as { [K in keyof T]: NameWithKnownLocale<T[K]> };
 		let feature: keyof T;
 		for (feature in obj) {
-			res[feature] = Object.assign({}, obj[feature], { name: obj[feature].name[lang] });
+			const localeName = obj[feature].name;
+			const name = typeof localeName === "string" ? localeName : localeName[lang];
+			res[feature] = Object.assign({}, obj[feature], { name });
 		}
 		return res;
 	}
 
-	public ofLanguage(lang: Language): ProfileConfig<IdT, ProductT, OptionT> {
+	/**
+	 * get the profile spec primed for a language
+	 * @param lang
+	 * @sealed
+	 */
+	public ofLanguage(lang: Language): ProfileConfig<IdT, ProductT, OptionT[number]> {
+		const opt = this.options.reduce<Pick<Options, OptionT[number]>>(
+			(acc, key: OptionT[number]) => {
+				acc[key] = options[key];
+				return acc;
+			},
+			{} as Pick<Options, OptionT[number]>
+		);
+
 		return {
 			name: this.name[lang],
 			id: this.id,
 			products: this.assignLangNames(lang, this.products),
-			options: this.assignLangNames(lang, this.options)
+			options: this.assignLangNames(lang, opt)
 		};
 	}
 }
