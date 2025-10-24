586a6169e7c3cf9995078991bf3ca66353679c54
dabund24 <dev@dabund24.de>
2025-10-24 14:08:52 +0200

    make `availableOptions` static member of profile class; add possible values

--- src/routes/[lang]/[profile]/api/profile/profile.server.ts
+++ src/routes/[lang]/[profile]/api/profile/profile.server.ts
@@ -6,22 +6,27 @@ export type Language = "de";
 
 type LocaleString = Record<Language, string>;
 
-type OptionConfig = {
-	/** human-readable, short description */
-	name: LocaleString;
-	defaultValue: string | number | boolean;
+type OptionValues = {
+	bike: boolean;
+	accessible: boolean;
+	maxTransfers: [0, 1, 2, 3, 4, 5, -1];
+	minTransferTime: [0, 2, 5, 10, 15, 20, 30, 40, 50, 60];
 };
 
-/**
- * all options ever available. A profile uses a subset of those
- */
-const options = {
-	bike: { defaultValue: false, name: { de: "Fahrradmitnahme" } },
-	accessible: { defaultValue: false, name: { de: "Barrierefreies Reisen" } },
-	maxTransfers: { defaultValue: -1, name: { de: "Maximale Umstiegsanzahl" } },
-	minTransferTime: { defaultValue: 0, name: { de: "Mindestumsteigezeit" } }
-} as const satisfies Record<string, OptionConfig>;
-type Options = typeof options;
+type OptionConfig<T extends string[] | number[] | boolean> = {
+	name: LocaleString;
+	defaultValue: T extends number[] | string[] ? T[number] : boolean;
+} & (T extends boolean
+	? Record<never, never>
+	: AssertUniqueTuple<T extends number[] | string[] ? T : never> extends never
+		? never
+		: {
+				possibleValues: T;
+				optionNames: Record<
+					T extends number[] | string[] ? T[number] : never,
+					LocaleString
+				>;
+			});
 
 type ProductConfig = {
 	name: LocaleString | string;
@@ -31,17 +36,17 @@ type NameWithKnownLocale<T extends { name: LocaleString | string }> = Omit<T, "n
 	name: string;
 };
 
-type ProfileConfig<
+export type ProfileConfig<
 	IdT extends ProfileId,
 	ProductT extends Product,
-	OptionT extends keyof Options
+	OptionT extends keyof typeof Profile.availableOptions
 > = {
 	/** very short identifier used for urls */
 	id: IdT;
 	/** human-readable, unique profile name; ideally the city or region where it can be used */
 	name: string;
 	products: Record<ProductT, NameWithKnownLocale<ProductConfig>>;
-	options: { [K in OptionT]: NameWithKnownLocale<Options[K]> };
+	options: { [K in OptionT]: NameWithKnownLocale<(typeof Profile.availableOptions)[K]> };
 };
 
 /**
@@ -49,9 +54,53 @@ type ProfileConfig<
  */
 export abstract class Profile<
 	IdT extends ProfileId,
-	ProductT extends Product,
-	OptionT extends readonly (keyof Options)[]
+	ProductT extends Product = Product,
+	OptionT extends
+		readonly (keyof typeof Profile.availableOptions)[] = readonly (keyof typeof Profile.availableOptions)[]
 > {
+	/** all options ever available. a profile uses a subset of those */
+	static readonly availableOptions = {
+		bike: {
+			name: { de: "Fahrradmitnahme" },
+			defaultValue: false
+		},
+		accessible: {
+			name: { de: "Barrierefreies Reisen" },
+			defaultValue: false
+		},
+		maxTransfers: {
+			name: { de: "Maximale Umstiegs-Anzahl" },
+			possibleValues: [0, 1, 2, 3, 4, 5, -1],
+			defaultValue: -1,
+			optionNames: {
+				0: { de: "Direkt-Verbindungen" },
+				1: Profile.translingual("1"),
+				2: Profile.translingual("2"),
+				3: Profile.translingual("3"),
+				4: Profile.translingual("4"),
+				5: Profile.translingual("5"),
+				[-1]: { de: "beliebig" }
+			}
+		},
+		minTransferTime: {
+			name: { de: "Mindest-Umsteigezeit" },
+			possibleValues: [0, 2, 5, 10, 15, 20, 30, 40, 50, 60],
+			defaultValue: 0,
+			optionNames: {
+				0: Profile.translingual("0min"),
+				2: Profile.translingual("2min"),
+				5: Profile.translingual("5min"),
+				10: Profile.translingual("10min"),
+				15: Profile.translingual("15min"),
+				20: Profile.translingual("20min"),
+				30: Profile.translingual("30min"),
+				40: Profile.translingual("40min"),
+				50: Profile.translingual("50min"),
+				60: Profile.translingual("1h")
+			}
+		}
+	} as const satisfies { [K in keyof OptionValues]: OptionConfig<OptionValues[K]> };
+
 	/** very short identifier used for urls */
 	protected abstract readonly id: IdT;
 	/** human-readable, unique profile name; ideally the city or region where it can be used */
@@ -79,18 +128,22 @@ export abstract class Profile<
 		return res;
 	}
 
+	protected static translingual(name: string): LocaleString {
+		return { de: name };
+	}
+
 	/**
 	 * get the profile spec primed for a language
 	 * @param lang
 	 * @sealed
 	 */
 	public ofLanguage(lang: Language): ProfileConfig<IdT, ProductT, OptionT[number]> {
-		const opt = this.options.reduce<Pick<Options, OptionT[number]>>(
+		const opt = this.options.reduce<Pick<typeof Profile.availableOptions, OptionT[number]>>(
 			(acc, key: OptionT[number]) => {
-				acc[key] = options[key];
+				acc[key] = Profile.availableOptions[key];
 				return acc;
 			},
-			{} as Pick<Options, OptionT[number]>
+			{} as Pick<typeof Profile.availableOptions, OptionT[number]>
 		);
 
 		return {
