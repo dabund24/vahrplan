b393a85718790cf37184cbad1b5b3f5b0e2d2931
dabund24 <dev@dabund24.de>
2025-10-24 14:07:48 +0200

    add factory function to proffile factory

--- src/routes/[lang]/[profile]/api/profile/profileFactory.server.ts
+++ src/routes/[lang]/[profile]/api/profile/profileFactory.server.ts
@@ -1,6 +1,13 @@
-import type { Profile, ProfileId } from "./profile.server";
+import type { Language, Profile, ProfileId } from "./profile.server";
 import { DbnavProfile } from "./dbnavProfile.server";
 
 const profiles = {
 	dbnav: new DbnavProfile()
 } as const satisfies { [K in ProfileId]: Profile<K> };
+
+export function profile<T extends ProfileId>(
+	id: T,
+	lang: Language
+): ReturnType<(typeof profiles)[T]["ofLanguage"]> {
+	return profiles[id].ofLanguage(lang) as ReturnType<(typeof profiles)[T]["ofLanguage"]>; // I have no idea why this assertion would be necessary
+}
