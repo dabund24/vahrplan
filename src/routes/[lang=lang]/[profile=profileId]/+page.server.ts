1e238ee210e337afd099f271dcfbf5e5199f6771
dabund24 <dev@dabund24.de>
2025-12-06 18:30:43 +0100

    dynamic entry generator

--- src/routes/[lang=lang]/[profile=profileId]/+page.server.ts
+++ src/routes/[lang=lang]/[profile=profileId]/+page.server.ts
@@ -1,8 +1,7 @@
 import type { EntryGenerator } from "./$types";
+import { profileIds } from "../../../params/profileId";
 
-export const entries: EntryGenerator = () => {
-	return [{ lang: "de", profile: "dbnav" }];
-};
+export const entries: EntryGenerator = () => profileIds.map((id) => ({ lang: "de", profile: id }));
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const prerender = true;
