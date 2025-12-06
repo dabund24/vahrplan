5d89b260a41e1369c3889306cf069a8840e12162
dabund24 <dev@dabund24.de>
2025-12-06 19:53:13 +0100

    move profile implementations to separate directory

--- src/lib/server/profiles/profile-implementations/bvgProfile.ts
+++ src/lib/server/profiles/profile-implementations/bvgProfile.ts
@@ -1,4 +1,4 @@
-import { Profile } from "./profile";
+import { Profile } from "../profile";
 import {
 	FptfDataService,
 	type FptfOptionId
--- src/lib/server/profiles/profileRegistry.ts
+++ src/lib/server/profiles/profileRegistry.ts
@@ -1,9 +1,9 @@
 import type { Profile, ProfileConfig } from "./profile";
-import { DbnavProfile } from "./dbnavProfile";
+import { DbnavProfile } from "./profile-implementations/dbnavProfile";
 import type { ProfileId } from "../../../params/profileId";
 import type { Language } from "../../../params/lang";
-import { EmptyProfile } from "./emptyProfile";
-import { BvgProfile } from "$lib/server/profiles/bvgProfile";
+import { EmptyProfile } from "./profile-implementations/emptyProfile";
+import { BvgProfile } from "$lib/server/profiles/profile-implementations/bvgProfile";
 
 const profiles = {
 	empty: new EmptyProfile(),
