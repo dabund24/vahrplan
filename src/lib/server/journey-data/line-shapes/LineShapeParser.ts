be32b04d68e54beb561cbcd269cc3084eea48738
dabund24 <dev@dabund24.de>
2025-12-18 15:38:11 +0100

    Fix line shapes loading too late (#209)
    * load line shapes synchronously

--- src/lib/server/journey-data/line-shapes/LineShapeParser.ts
+++ src/lib/server/journey-data/line-shapes/LineShapeParser.ts
@@ -13,79 +13,73 @@ export type LineShape = {
 	shape: "circle" | "hexagon" | "rectangle" | "rectangle-rounded-corner" | "pill" | "trapezoid";
 };
 
+type TraewellingLineShape = LineShape & {
+	shortOperatorName: string;
+	operatorCode?: string;
+	lineId: string;
+	["delfiAgencyID"]: string;
+	delfiAgencyName: string;
+};
+
+const traewellingLineShapes = await buildTraewellingLineShapes();
+
+async function buildTraewellingLineShapes(): Promise<TraewellingLineShape[]> {
+	const result: TraewellingLineShape[] = [];
+
+	const csvLines = await lineShapeCsvIterator(lineShapesCsv);
+
+	for (const line of csvLines) {
+		const [
+			shortOperatorName,
+			lineName,
+			operatorCode,
+			lineId,
+			backgroundColor,
+			textColor,
+			borderColor,
+			shape,
+			_wikidataQid,
+			delfiAgencyId,
+			delfiAgencyName,
+		] = line.split(",");
+
+		const lineCodeEntry = {
+			shortOperatorName,
+			lineName,
+			operatorCode: operatorCode === "" ? undefined : operatorCode,
+			lineId,
+			background: { type: "fixed", value: backgroundColor },
+			text: { type: "fixed", value: textColor },
+			border:
+				borderColor === "" ? undefined : ({ type: "fixed", value: borderColor } as const),
+			["delfiAgencyID"]: delfiAgencyId,
+			delfiAgencyName,
+			shape: shape as LineShape["shape"],
+		} as const;
+		result.push(lineCodeEntry);
+	}
+
+	return result;
+}
+
+/**
+ * iterate over csv
+ * @param route
+ */
+async function lineShapeCsvIterator(route: string): Promise<Iterable<string>> {
+	const fileContent = await read(route).text();
+	const lines = fileContent.split("\n");
+	lines.shift(); // remove first element
+	lines.pop(); // remove last element
+	return lines;
+}
+
 /**
  * extend to parse line shapes
  * @typeParam T - the type line details should be parsed from
  */
 export abstract class LineShapeParser<T> {
-	private static readonly buildTraewellingLineShapes = async (): Promise<
-		typeof this.traewellingLineShapes
-	> => {
-		const result: typeof this.traewellingLineShapes = [];
-
-		const csvLines = await this.lineShapeCsvIterator(lineShapesCsv);
-
-		for (const line of csvLines) {
-			const [
-				shortOperatorName,
-				lineName,
-				operatorCode,
-				lineId,
-				backgroundColor,
-				textColor,
-				borderColor,
-				shape,
-				_wikidataQid,
-				delfiAgencyId,
-				delfiAgencyName,
-			] = line.split(",");
-
-			const lineCodeEntry = {
-				shortOperatorName,
-				lineName,
-				operatorCode: operatorCode === "" ? undefined : operatorCode,
-				lineId,
-				background: { type: "fixed", value: backgroundColor },
-				text: { type: "fixed", value: textColor },
-				border:
-					borderColor === ""
-						? undefined
-						: ({ type: "fixed", value: borderColor } as const),
-				["delfiAgencyID"]: delfiAgencyId,
-				delfiAgencyName,
-				shape: shape as LineShape["shape"],
-			} as const;
-			result.push(lineCodeEntry);
-		}
-
-		return result;
-	};
-
-	/**
-	 * iterate over csv
-	 * @param route
-	 */
-	private static lineShapeCsvIterator = async (route: string): Promise<Iterable<string>> => {
-		const fileContent = await read(route).text();
-		const lines = fileContent.split("\n");
-		lines.shift(); // remove first element
-		lines.pop(); // remove last element
-		return lines;
-	};
-
-	protected static traewellingLineShapes: (LineShape & {
-		shortOperatorName: string;
-		operatorCode?: string;
-		lineId: string;
-		["delfiAgencyID"]: string;
-		delfiAgencyName: string;
-	})[] = [];
-
-	static {
-		void this.buildTraewellingLineShapes().then(
-			(lineShapes) => (this.traewellingLineShapes = lineShapes),
-		);
-	}
+	protected static traewellingLineShapes: TraewellingLineShape[] = traewellingLineShapes;
 
 	protected readonly stringToNormalForm = (str: string): string =>
 		str.toLowerCase().replaceAll(" ", "");
