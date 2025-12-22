801982dcceec6e6e5a30e467c9a41d81a1b01ba2
dabund24 <dev@dabund24.de>
2025-12-22 12:16:37 +0100

    add tests

--- tests/unit/journey-data-service/fptf/location/bvgLocation.test.ts
+++ tests/unit/journey-data-service/fptf/location/bvgLocation.test.ts
@@ -1,7 +1,11 @@
 import { expect, test, vi } from "vitest";
 import { journeyDataService } from "$lib/server/profiles/profileRegistry";
 import type { HafasClient, Location, Station, Stop } from "hafas-client";
-import { bvgLocationParsedRes } from "./bvgLocation-parsedRes";
+import {
+	bvgLocationAddressParsedRes,
+	bvgLocationPoiParsedRes,
+	bvgLocationStationParsedRes,
+} from "./bvgLocation-parsedRes";
 import { bvgLocationFormattedReq } from "./bvgLocation-formattedReq";
 import { bvgLocationRawRes } from "./bvgLocation-rawRes";
 
@@ -9,7 +13,7 @@ vi.mock("$app/server", () => ({
 	read: (): object => ({ text: () => "" }),
 }));
 
-test("fptf data service location", async () => {
+test("fptf data service station location", async () => {
 	vi.mock("hafas-client", () => {
 		return {
 			createClient: (): Pick<HafasClient, "stop"> => ({
@@ -27,6 +31,30 @@ test("fptf data service location", async () => {
 	const res = await dataService.location("900100003", { lang: "de" });
 
 	expect(!res.isError && res.content, "response was parsed incorrectly").toEqual(
-		bvgLocationParsedRes,
+		bvgLocationStationParsedRes,
+	);
+});
+
+test("fptf data service address location", async () => {
+	const dataService = journeyDataService("bvg", "de");
+	const res = await dataService.location(
+		'{"type":"location","id":null,"latitude":52.517544,"longitude":13.397047,"address":"Unter den Linden, 10117 Berlin-Mitte"}',
+		{ lang: "de" },
+	);
+
+	expect(!res.isError && res.content, "response was parsed incorrectly").toEqual(
+		bvgLocationAddressParsedRes,
+	);
+});
+
+test("fptf data service poi location", async () => {
+	const dataService = journeyDataService("bvg", "de");
+	const res = await dataService.location(
+		'{"type":"location","id":"900981087","latitude":52.518614,"longitude":13.375535,"name":"Berlin, Reichstagsgebäude","poi":true}',
+		{ lang: "de" },
+	);
+
+	expect(!res.isError && res.content, "response was parsed incorrectly").toEqual(
+		bvgLocationPoiParsedRes,
 	);
 });
