6655e7f73a57b5aa0c759e66c7e10a8ee6bc1c1d
dabund24 <dev@dabund24.de>
2026-04-11 14:29:09 +0200

    toast after trip refresh

--- src/routes/[lang=lang]/[profile=profileId]/trip/[tripId]/tripUtils.ts
+++ src/routes/[lang=lang]/[profile=profileId]/trip/[tripId]/tripUtils.ts
@@ -2,6 +2,7 @@ import type { Trip } from "$lib/types";
 import type { DisplayedJourney } from "$lib/state/displayedJourney.svelte";
 import type { GetTripApiClient } from "../../api/trip/[tripId]/getClient";
 import { apiClient } from "$lib/api-client/apiClientFactory";
+import { toast } from "$lib/state/toastStore";
 
 export function tripToDisplayedJourney(trip: Trip): DisplayedJourney {
 	const departureTime = trip.leg.departureData.time.departure;
@@ -28,6 +29,7 @@ export async function refreshTrip(
 	const tripApiClient = apiClient("GET", "trip/[tripId]");
 	const res = await tripApiClient.request({ tripId, highlightData });
 	if (!res.isError) {
+		toast("Fahrtdaten aktualisiert.", "green");
 		return { trip: res.content, displayedJourney: tripToDisplayedJourney(res.content) };
 	}
 	return undefined;
