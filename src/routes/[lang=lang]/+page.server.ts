4668d8c82d4d55e712a220216507500c2d6281ef
dabund24 <dev@dabund24.de>
2026-06-07 19:55:45 +0200

    redirect to transitous profile by default

--- src/routes/[lang=lang]/+page.server.ts
+++ src/routes/[lang=lang]/+page.server.ts
@@ -1,6 +1,10 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { resolve } from "$app/paths";
 
 export const load: PageServerLoad = () => {
-	redirect(308, "/de/profiles");
+	redirect(
+		308,
+		resolve("/[lang=lang]/[profile=profileId]", { lang: "de", profile: "transitous" }),
+	);
 };
